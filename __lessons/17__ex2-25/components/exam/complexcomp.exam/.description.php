f7c9fc2d9bb3634fdeb4ddc272b9fca447d7a2aa
Pavel Polyanskiy <paxan95g@mail.ru>
2018-08-14 17:25:51 +1000

    17 ex

---
+++ __lessons/17__ex2-25/components/exam/complexcomp.exam/.description.php
@@ -0,0 +1,14 @@
+<?
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+$arComponentDescription = array(
+	"NAME" => GetMessage("IBLOCK_PHOTO_NAME"),
+	"DESCRIPTION" => GetMessage("IBLOCK_PHOTO_DESCRIPTION"),
+	"ICON" => "/images/photo.gif",
+	"COMPLEX" => "Y",
+	"PATH" => array(
+		"ID" => "exam",
+		"NAME" => GetMessage("IBLOCK_PHOTO_SECTION"),
+        "SORT" => 1,
+	),
+);
+?>
---
+++ __lessons/17__ex2-25/components/exam/complexcomp.exam/templates/.default/exampage.php
@@ -0,0 +1,4 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<p>Страница: <b>exampage</b></p>
+<p><?='PARAM1: '. $arResult['VARIABLES']['PARAM1'];?></p>
+<p><?='PARAM2: '. $arResult['VARIABLES']['PARAM2'];?></p>
